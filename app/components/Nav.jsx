539ac2f7f8779b80e787bce34bf356fd56a9a1b0
fergmac <f.macconnell@gmail.com>
2017-03-01 10:10:52 -0800

    update Nav component with custom scss

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -7,7 +7,7 @@ var Nav = React.createClass({
             <div className="top-bar">
                 <div className="top-bar-left">
                     <ul className="menu">
-                        <li>React Time App</li>
+                        <li className="menu-text">React Time App</li>
                         <li>
                             <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
                         </li>
